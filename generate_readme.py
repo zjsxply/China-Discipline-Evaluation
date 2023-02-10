bec93ebb2374693a4ed567984a55d19f599da29b
zjsxply <1609566137@qq.com>
2023-02-10 18:37:16 +0800

    minor changes

--- generate_readme.py
+++ generate_readme.py
@@ -24,13 +24,14 @@ def format_data(df, sort='学校'):
         if last != xx:
             last = xx
             result += f'\n### {xx}\n\n'
-            result += f'''| {the_other} | 等级/结果 | 推测等级或第四轮等级 |\n| -------------- | ------------------------------ | -------------------- |\n'''
+            result += f'''| {the_other} | 等级（原话） | 推测等级或第四轮等级 |\n| -------------- | ------------------------------ | -------------------- |\n'''
         result += f'| {xk} | {jg} | {tc} |\n'
     return result
 
 def generate():
     df = pd.read_table('original_data.csv', sep=",", encoding='gbk')
     df.fillna('', inplace=True)
+    data_count = df.shape[0]
     
     # 截取所需要的数据，并去重
     sorted_df = df[['学校','学科','等级/结果','推测等级或第四轮等级']]
@@ -39,10 +40,12 @@ def generate():
     # 排序并格式化数据，按学科排列时忽略非标准学科名称的行
     sorted_df['学校拼音'] = sorted_df['学校'].map(lambda x: tuple(lazy_pinyin(x)))
     sorted_df['学科代码'] = sorted_df['学科'].map(lambda x: constants.DISCIPLINE_CODE.get(x, np.nan))
-    order_by_school = format_data(sorted_df, sort='学校')
+    order_by_school = format_data(sorted_df.sort_values(by=['学校拼音', '学科代码'], na_position='first'), sort='学校')
     order_by_discipline = format_data(sorted_df.dropna().sort_values(by=['学科代码', '学校拼音']), sort='学科')
     
-    text = constants.TEMPLATE_README.format(order_by_school=order_by_school, order_by_discipline=order_by_discipline)
+    school_count = sorted_df['学校'].drop_duplicates().shape[0]
+    discipline_count = sorted_df['学科代码'].drop_duplicates().shape[0]
+    text = constants.TEMPLATE_README.format(order_by_school=order_by_school, order_by_discipline=order_by_discipline, school_count=school_count, discipline_count=discipline_count, data_count=data_count)
     return text
 
 with open('README.md', 'w') as f:
